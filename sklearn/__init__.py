93e78e99f3b87065415e3b6e2ef0b76be7bb1520
Arnaud Fouchet <arnaud.fouchet@dolead.com>
2014-11-13 16:21:40 +0100

    bump version

--- sklearn/__init__.py
+++ sklearn/__init__.py
@@ -15,7 +15,7 @@ See http://scikit-learn.org for complete documentation.
 import sys
 import re
 import warnings
-__version__ = '0.17-git'
+__version__ = '0.17.1-git'
 
 # Make sure that DeprecationWarning within this package always gets printed
 warnings.filterwarnings('always', category=DeprecationWarning,
