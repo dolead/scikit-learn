92b9c014de08489ed02f410e824a29bb1bc3f7e1
Arnaud Fouchet <arnaud.fouchet@dolead.com>
2015-02-19 16:13:42 +0100

    add unittest for information criteria
    gaussian log likelihood
    AIC
    BIC

--- sklearn/cluster/tests/test_choose_nb_cluster.py
+++ sklearn/cluster/tests/test_choose_nb_cluster.py
@@ -1,10 +1,12 @@
 import numpy as np
+from math import log
 
 from sklearn.utils.testing import assert_array_equal, assert_almost_equal, assert_equal
 
 from sklearn.cluster.choose_nb_cluster import (
     adjacency_matrix, _one_stability_measure, stability, distortion,
     normal_distortion, gap_statistic, distortion_jump, max_silhouette,
+    AIC, BIC, gaussian_log_likelihood,
 )
 from sklearn.cluster.k_means_ import k_means
 
@@ -54,21 +56,21 @@ def test_normal_distortion():
         n = len(points)
         mid = n / 2
         return [int(i < mid) for i in range(n)]
-    assert_almost_equal(normal_distortion(np.zeros((100, 2)), clu_meth,
-                                          nb_draw=10, random_state=0),
-                        93.731154)
+    assert_almost_equal(np.mean(normal_distortion(
+        np.zeros((100, 2)), clu_meth, nb_draw=10, random_state=0)),
+                        0.93731154)
 
 
 def test_gap_statistic():
     generator = np.random.RandomState(0)
     # for j in [20 * i: 20 * (i+1)[, x[j] = [rand rand] + [4 * i, 4 * i]
-    X = generator.uniform(size=(60, 2))
-    offset = np.dot(np.arange(60).reshape((60, 1)) / 20 * 4, np.ones((1, 2)))
+    X = generator.uniform(size=(100, 2))
+    offset = np.dot(np.arange(100).reshape((100, 1)) / 20 * 4, np.ones((1, 2)))
     X += offset
     clu_meth = lambda X, k: k_means(X, k, random_state=1)[1]
-    assert_equal(gap_statistic(X, clu_meth, k_max=6, nb_draw=10, random_state=0), 3)
-    assert_equal(gap_statistic(X, clu_meth, k_max=6, nb_draw=10, random_state=0,
-                               draw_model='normal'), 3)
+    assert_equal(gap_statistic(X, clu_meth, k_max=10, nb_draw=10, random_state=0), 5)
+    assert_equal(gap_statistic(X, clu_meth, k_max=10, nb_draw=10, random_state=0,
+                               draw_model='normal'), 5)
 
 
 def test_distortion_jump():
@@ -89,3 +91,32 @@ def test_silhouette():
     X += offset
     clu_meth = lambda X, k: k_means(X, k, random_state=1)[1]
     assert_equal(max_silhouette(X, clu_meth, k_max=6), 5)
+
+
+def test_aic():
+    generator = np.random.RandomState(0)
+    # for j in [20 * i: 20 * (i+1)[, x[j] = [rand rand] + [10 * i, 10 * i]
+    X = generator.uniform(size=(80, 2))
+    offset = np.dot(np.arange(80).reshape((80, 1)) / 20 * 10, np.ones((1, 2)))
+    X += offset
+    clu_meth = lambda X, k: k_means(X, k, random_state=1)[1]
+    assert_equal(AIC(X, clu_meth, k_max=6), 6)
+
+
+def test_bic():
+    generator = np.random.RandomState(0)
+    # for j in [20 * i: 20 * (i+1)[, x[j] = [rand rand] + [10 * i, 10 * i]
+    X = generator.uniform(size=(80, 2))
+    offset = np.dot(np.arange(80).reshape((80, 1)) / 20 * 10, np.ones((1, 2)))
+    X += offset
+    clu_meth = lambda X, k: k_means(X, k, random_state=1)[1]
+    assert_equal(BIC(X, clu_meth, k_max=6), 6)
+
+
+def test_gaussian_log_likelihood():
+    generator = np.random.RandomState(0)
+    X = generator.multivariate_normal(
+        np.zeros(2), np.eye(2, 2), size=1000)
+    assi = np.arange(1000) / 500
+    assert_almost_equal(gaussian_log_likelihood(X, assi), -966.993 * log(2),
+                        decimal=2)
