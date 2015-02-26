d6c0b076ea7fc574a3c98e7eed14c5a1510c4cc1
Arnaud Fouchet <arnaud.fouchet@dolead.com>
2015-02-26 16:44:09 +0100

    add Calinski Harabaz index calc

--- sklearn/cluster/tests/test_choose_nb_cluster.py
+++ sklearn/cluster/tests/test_choose_nb_cluster.py
@@ -6,6 +6,7 @@ from sklearn.utils.testing import (assert_array_equal, assert_almost_equal,
 from sklearn.cluster.choose_nb_cluster import (
     adjacency_matrix, _one_stability_measure, stability, distortion,
     normal_distortion, gap_statistic, distortion_jump, max_silhouette,
+    calc_CH_index, max_CH_index,
 )
 from sklearn.cluster.k_means_ import k_means
 
@@ -91,3 +92,20 @@ def test_silhouette():
     X += offset
     clu_meth = lambda X, k: k_means(X, k, random_state=1)[1]
     assert_equal(max_silhouette(X, clu_meth, k_max=6), 5)
+
+
+def test_calc_CH():
+    X = np.array([[0, 0]] * 50 + [[1, 1]] * 50
+                 + [[3, 3]] * 50 + [[4, 4]] * 50)
+    labels = [0] * 100 + [1] * 100
+    assert_equal(calc_CH_index(X, labels), 4.5 * (200 - 2) / .5)
+
+
+def test_Calinsk_Harabasz():
+    generator = np.random.RandomState(0)
+    # for j in [20 * i: 20 * (i+1)[, x[j] = [rand rand] + [4 * i, 4 * i]
+    X = generator.uniform(size=(100, 2))
+    offset = np.dot(np.arange(100).reshape((100, 1)) / 20 * 1, np.ones((1, 2)))
+    X += offset
+    clu_meth = lambda X, k: k_means(X, k, random_state=1)[1]
+    assert_equal(max_CH_index(X, clu_meth, k_max=6), 5)
